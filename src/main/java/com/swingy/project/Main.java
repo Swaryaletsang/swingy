87d0bb785a80b9985bcc0869b1e5bbf60fabe289
Swaryaletsang <atau@student.wethinkcode.co.za>
2020-09-21 18:45:59 -0700

    monday night

--- src/main/java/com/swingy/project/Main.java
+++ src/main/java/com/swingy/project/Main.java
@@ -1,5 +1,7 @@
 package com.swingy.project;
 
+import org.slf4j.*;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,15 @@ public class Main
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        if (args[0].equals("console")) {
+            //simulate console view (while loop)
+        } else if (args[0].equals("gui")) {
+            // simulate GUI view (while loop)
+        }
+         else {
+            //error message
+        }
+        Logger log = LoggerFactory.getLogger(Main.class);
+        log.info("Hello world");
     }
 }
